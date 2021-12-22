bac3f8212882d2e0945ce9e4d1fe4e375774e472
Sulav Shrestha <gilm.dav007@gmail.com>
2021-12-22 16:18:33 +0545

    [Type Table]
    - Implement List and Update for Type table

--- methods/type.go
+++ methods/type.go
@@ -2,6 +2,8 @@ package methods
 
 import (
 	"context"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	question "questionService/libs"
 	"questionService/models"
@@ -26,5 +28,53 @@ func (*Server) CreateType(ctx context.Context, req *question.CreateTypeRequest) 
 	}
 
 	response := &question.CreateTypeResponse{Type: &data}
+	return response, nil
+}
+
+func (*Server) UpdateType(ctx context.Context, req *question.UpdateTypeRequest) (*question.UpdateTypeResponse, error) {
+	reqData := req.GetType()
+
+	data := question.Type{
+		Name:        reqData.Name,
+		CreatedById: reqData.CreatedById,
+		CreatedDate: reqData.CreatedDate,
+		UpdatedDate: reqData.UpdatedDate,
+		Active:      reqData.Active,
+	}
+	oid := req.GetId()
+
+	err := _type.Update(models.Dbclient, &data, oid)
+	if err != nil {
+		return nil, err
+	}
+	response := &question.UpdateTypeResponse{Type: &data}
+	return response, nil
+}
+
+func (*Server) ListType(ctx context.Context, req *question.ListTypeRequest) (*question.ListTypeResponse, error) {
+	rows, err := _type.List(models.Dbclient)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "Unexpected Error Occurred: %v", err)
+	}
+	var i *uint32
+	var responses []*question.Type
+	defer func() {
+		responses = nil
+		i = nil
+	}()
+
+	for rows.Next() {
+		data := question.Type{}
+		err = rows.Scan(&i, &data.Name, &data.CreatedById, &data.CreatedDate, &data.UpdatedDate, &data.Active)
+		if err != nil {
+			log.Printf("Error when getting the object %v", err.Error())
+			return nil, err
+		}
+		responses = append(responses, &data)
+	}
+
+	response := &question.ListTypeResponse{
+		Type: responses,
+	}
 	return response, nil
 }
--- models/actions/type/type.go
+++ models/actions/type/type.go
@@ -3,6 +3,8 @@ package _type
 import (
 	"context"
 	"database/sql"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"log"
 	question "questionService/libs"
 	"time"
@@ -30,4 +32,34 @@ func Insert(db *sql.DB, t *question.Type) (int64, error) {
 	}
 	log.Printf("Object with ID %d created", id)
 	return id, nil
+}
+
+func Update(db *sql.DB, t *question.Type, oid uint32) error {
+	query := "UPDATE type SET name=?, created_by_id=?, created_date=?, updated_date=?, active=? WHERE id=?"
+
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Error updating: %v", err)
+	}
+	res, err := stmt.Exec(t.Name, t.CreatedById, t.CreatedDate, t.UpdatedDate, t.Active, oid)
+	if err != nil {
+		return status.Errorf(codes.Internal, "Error saving: %v", err)
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return status.Errorf(codes.Internal, "Error getting rows affected: %v", err)
+	}
+	log.Printf("Rows affected: %v", rows)
+	return nil
+}
+
+func List(db *sql.DB) (*sql.Rows, error) {
+	query := "SELECT * FROM type"
+	_, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	rows, err := db.Query(query)
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
 }
